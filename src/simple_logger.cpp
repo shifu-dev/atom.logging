68a894d4a2942f7820fd08c68a76c67b2a192b65
shifu-dev <csingh8476@gmail.com>
2024-02-24 15:20:36 +0530

    update: update simple_logger.

--- src/simple_logger.cpp
+++ src/simple_logger.cpp
@@ -2,166 +2,320 @@ export module atom.logging:simple_logger;
 import :log_msg;
 import :logger;
 import :log_target;
-import :multi_log_target;
 import atom.core;
 
 namespace atom::logging
 {
-    /// --------------------------------------------------------------------------------------------
-    /// --- doc_template
-    /// --------------------------------------------------------------------------------------------
-    /// basic logger object.
-    ///
-    /// @thread_safety safe
-    /// --------------------------------------------------------------------------------------------
     template <bool st>
-    class simple_logger_template: public logger
+    class _simple_logger_impl
     {
-        using multi_log_target =
-            tti::conditional_type<st, multi_log_target_st, multi_log_target_mt>;
-        using atomic_log_level = tti::conditional_type<st, log_level, atomic<log_level>>;
+        using this_type = _simple_logger_impl;
+        using atomic_log_level_type = tti::conditional_type<st, log_level, atomic<log_level>>;
+        using lockable_type = tti::conditional_type<st, null_lockable, simple_mutex>;
 
     public:
-        /// ----------------------------------------------------------------------------------------
-        /// default_constructor.
-        ///
-        /// @param[in] name name of this logger.
-        /// ----------------------------------------------------------------------------------------
-        explicit simple_logger_template(string name)
+        explicit _simple_logger_impl(string name)
             : _name(move(name))
-            , targets()
+            , _targets()
         {}
 
-        /// ----------------------------------------------------------------------------------------
-        /// constructs and adds log_target objects.
-        /// constructs {targets(targets)}.
-        ///
-        /// @param[in] name name of this logger.
-        /// @param[in] targets log_target objects to add.
-        /// ----------------------------------------------------------------------------------------
-        template <typename trange>
-        simple_logger_template(string name, const trange& targets)
-            requires(rrange_of<trange, log_target*>)
+        template <typename range_type>
+        _simple_logger_impl(string name, range_type&& targets)
+            requires(rrange_of<pure_type<range_type>, log_target*>)
             : _name(move(name))
-            , targets(targets)
+            , _targets(forward<range_type>(targets))
         {}
+
+        _simple_logger_impl(const this_type& that) = delete;
+        _simple_logger_impl& operator=(const this_type&& that) = delete;
+
+        _simple_logger_impl(this_type&& that) = delete;
+        _simple_logger_impl& operator=(this_type&& that) = delete;
+
+        ~_simple_logger_impl() {}
 
     public:
-        /// ----------------------------------------------------------------------------------------
-        /// get the name of this logger.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
-        auto get_name() const -> string_view override final
+        auto get_name() const -> string_view
         {
             return _name;
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// writes to all log_target objects.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
-        auto log(log_msg& log_msg) -> void override
+        auto log(log_msg& log_msg) -> void
         {
-            targets.write(log_msg);
+            lock_guard guard(_lock);
+
+            for (log_target* target : _targets)
+            {
+                target->write(log_msg);
+            }
+
+            if (check_flush_level(log_msg.lvl))
+            {
+                for (log_target* target : _targets)
+                {
+                    target->flush();
+                }
+            }
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// flushes all log_target objects.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
-        auto flush() -> void override
+        auto flush() -> void
         {
-            targets.flush();
+            lock_guard guard(_lock);
+
+            for (log_target* target : _targets)
+            {
+                target->flush();
+            }
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// set log level.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
         auto set_log_level(log_level lvl)
         {
             _log_level = lvl;
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// get log level.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
         auto get_log_level(log_level lvl) -> log_level
         {
             return _log_level;
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// checks if log of level {lvl} should be logged.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
-        auto check_log_level(log_level lvl) const -> bool override final
+        auto check_log_level(log_level lvl) const -> bool
         {
             return lvl != log_level::off && lvl >= _log_level;
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// set flush level.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
         auto set_flush_level(log_level lvl)
         {
             _flush_level = lvl;
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// get flush level.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
         auto get_flush_level() const -> log_level
         {
             return _flush_level;
         }
 
-        /// ----------------------------------------------------------------------------------------
-        /// checks if log of level {lvl} should be flushed.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
         auto check_flush_level(log_level lvl) const -> bool
         {
             return lvl != log_level::off && lvl >= _flush_level;
         }
 
-    public:
-        /// ----------------------------------------------------------------------------------------
-        /// the object used to manage log_target objects. accessing it is thread safe.
-        ///
-        /// @thread_safety safe
-        /// ----------------------------------------------------------------------------------------
-        multi_log_target targets;
+        auto add_target(log_target* target) -> void
+        {
+            lock_guard guard(_lock);
+            _targets.emplace_back(target);
+        }
 
-    protected:
-        /// ----------------------------------------------------------------------------------------
-        /// name of this logger.
-        /// ----------------------------------------------------------------------------------------
+        template <typename range_type>
+        auto add_targets(range_type&& targets) -> void
+            requires rrange_of<pure_type<range_type>, log_target*>
+        {
+            lock_guard guard(_lock);
+            _targets.insert_range_back(forward<range_type>(targets));
+        }
+
+        auto remove_target(log_target* target) -> void
+        {
+            lock_guard guard(_lock);
+            _targets.remove_find(target);
+        }
+
+        auto has_target(log_target* target) const -> bool
+        {
+            lock_guard guard(_lock);
+            for (log_target* value : _targets)
+            {
+                if (value == target)
+                    return true;
+            }
+
+            return false;
+        }
+
+        auto get_targets() const -> array_view<log_target*>
+        {
+            return _targets;
+        }
+
+    private:
         const string _name;
-
-        /// ----------------------------------------------------------------------------------------
-        /// log_level used to filter logs.
-        /// ----------------------------------------------------------------------------------------
-        atomic_log_level _log_level;
-
-        /// ----------------------------------------------------------------------------------------
-        /// log_level used to filter flush after logs.
-        /// ----------------------------------------------------------------------------------------
-        atomic_log_level _flush_level;
+        atomic_log_level_type _log_level;
+        atomic_log_level_type _flush_level;
+        dynamic_array<log_target*> _targets;
+        lockable_type _lock;
     };
 
-    export using simple_logger_st = simple_logger_template<true>;
-    export using simple_logger_mt = simple_logger_template<false>;
+    using _simple_logger_impl_st = _simple_logger_impl<true>;
+    using _simple_logger_impl_mt = _simple_logger_impl<false>;
+
+    /// --------------------------------------------------------------------------------------------
+    /// basic logger type.
+    /// --------------------------------------------------------------------------------------------
+    template <typename impl_type>
+    class simple_logger: public logger
+    {
+    public:
+        /// ----------------------------------------------------------------------------------------
+        /// # default constructor.
+        ///
+        /// @param[in] name name of this logger.
+        /// ----------------------------------------------------------------------------------------
+        explicit simple_logger(string name)
+            : _impl(move(name))
+        {}
+
+        /// ----------------------------------------------------------------------------------------
+        /// constructs and adds `log_target` objects.
+        ///
+        /// @param[in] name name of this logger.
+        /// @param[in] targets `log_target` objects to add.
+        /// ----------------------------------------------------------------------------------------
+        template <typename range_type>
+        simple_logger(string name, range_type&& targets)
+            requires(rrange_of<pure_type<range_type>, log_target*>)
+            : _impl(move(name), forward<range_type>(targets))
+        {}
+
+        /// ----------------------------------------------------------------------------------------
+        /// constructs and adds `log_target` objects.
+        ///
+        /// @param[in] name name of this logger.
+        /// @param[in] targets `log_target` objects to add.
+        /// ----------------------------------------------------------------------------------------
+        simple_logger(string name, initializer_list<log_target*> targets)
+            : _impl(move(name), make_range(targets))
+        {}
+
+        /// ----------------------------------------------------------------------------------------
+        /// # destructor
+        /// ----------------------------------------------------------------------------------------
+        ~simple_logger() {}
+
+    public:
+        /// ----------------------------------------------------------------------------------------
+        /// get the name of this logger.
+        /// ----------------------------------------------------------------------------------------
+        auto get_name() const -> string_view override final
+        {
+            return _impl.get_name();
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// writes to all log_target objects.
+        /// ----------------------------------------------------------------------------------------
+        auto log(log_msg& log_msg) -> void override
+        {
+            _impl.log(log_msg);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// flushes all log_target objects.
+        /// ----------------------------------------------------------------------------------------
+        auto flush() -> void override
+        {
+            _impl.flush();
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// sets log level.
+        /// ----------------------------------------------------------------------------------------
+        auto set_log_level(log_level lvl)
+        {
+            _impl.set_log_level(lvl);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// gets log level.
+        /// ----------------------------------------------------------------------------------------
+        auto get_log_level(log_level lvl) -> log_level
+        {
+            return _impl.get_log_level();
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// checks if `lvl` is high enough to pass log level filter.
+        /// ----------------------------------------------------------------------------------------
+        auto check_log_level(log_level lvl) const -> bool override final
+        {
+            return _impl.check_log_level(lvl);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// sets flush level.
+        /// ----------------------------------------------------------------------------------------
+        auto set_flush_level(log_level lvl)
+        {
+            _impl.set_flush_level(lvl);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// gets flush level.
+        /// ----------------------------------------------------------------------------------------
+        auto get_flush_level() const -> log_level
+        {
+            return _impl.get_flush_level();
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// checks if `lvl` is high enough to pass flush level filter.
+        /// ----------------------------------------------------------------------------------------
+        auto check_flush_level(log_level lvl) const -> bool
+        {
+            return _impl.check_flush_level(lvl);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        ///
+        /// ----------------------------------------------------------------------------------------
+        auto add_target(log_target* target) -> void
+        {
+            return _impl.add_target(target);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        ///
+        /// ----------------------------------------------------------------------------------------
+        template <typename range_type>
+        auto add_targets(range_type&& targets) -> void
+            requires rrange_of<pure_type<range_type>, log_target*>
+        {
+            return _impl.add_targets(forward<range_type>(targets));
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        ///
+        /// ----------------------------------------------------------------------------------------
+        auto add_targets(initializer_list<log_target*> targets) -> void
+        {
+            return _impl.add_targets(make_range(targets));
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        ///
+        /// ----------------------------------------------------------------------------------------
+        auto remove_target(log_target* target) -> void
+        {
+            return _impl.remove_target(target);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        ///
+        /// ----------------------------------------------------------------------------------------
+        auto get_targets() const -> array_view<log_target*>
+        {
+            return _impl.get_targets();
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        ///
+        /// ----------------------------------------------------------------------------------------
+        auto has_target(log_target* target) const -> bool
+        {
+            return _impl.has_target(target);
+        }
+
+    private:
+        impl_type _impl;
+    };
+
+    export using simple_logger_st = simple_logger<_simple_logger_impl_st>;
+    export using simple_logger_mt = simple_logger<_simple_logger_impl_mt>;
 }
